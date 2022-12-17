dc89e2034d41c972e8cee4619279db4ba476103f
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-17 22:15:21 +0000

    feat: added django cleanup to isntalled apps

--- backend/settings.py
+++ backend/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
