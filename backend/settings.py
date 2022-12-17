2b52bc3c9de5c367909f5c29c9274c2508f82d5f
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-17 22:26:52 +0000

    feat: added support for django cors headers

--- backend/settings.py
+++ backend/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "corsheaders",
     'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
