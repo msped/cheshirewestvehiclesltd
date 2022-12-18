6eeef63609bf8bb2f3bf5806e22421710923fd7d
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-18 19:42:03 +0000

    refactor: added published field to gallery tiem

--- gallery/models.py
+++ gallery/models.py
@@ -11,6 +11,7 @@ class GalleryItem(models.Model):
     trim = models.CharField(max_length=45)
     year = models.IntegerField()
     description = models.TextField()
+    published = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.id} {self.make} {self.model} {self.trim}'
