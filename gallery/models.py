5626100cda9c73d51ce564bdb4d16b9f4cbcf465
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-18 18:55:53 +0000

    feat: added GalleryItem and GalleryImage models

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+class GalleryItem(models.Model):
+    """Gallery Item"""
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    make = models.CharField(max_length=45)
+    model = models.CharField(max_length=45)
+    trim = models.CharField(max_length=45)
+    year = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.id} {self.make} {self.model} {self.trim}'
+
+    class Meta:
+        ordering = ['-id']
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(f'{self.make} {self.model} {self.trim} {self.year}')
+        super(GalleryItem).save(*args, **kwargs)
+
+class GalleryImage(models.Model):
+    """Image for galleryItem"""
+    item = models.ForeignKey(GalleryItem, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='gallery')
+    order_of_images = models.IntegerField()
+
+    class Meta:
+        ordering = ['order_of_images']
+
+    def __str__(self):
+        return f'{self.id} {self.item}'
