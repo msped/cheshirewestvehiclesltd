75b090dc36ace0febcf00231d1e81658f69e6110
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-21 20:52:09 +0000

    feat: added gallery detail view

--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,15 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
-from .models import GalleryItem, GalleryImage
+from .models import GalleryItem
 from .serializers import GallerySerializer
 
 class GalleryList(ListAPIView):
     serializer_class = GallerySerializer
     paginate_by = 10
     queryset = GalleryItem.objects.filter(published=True)
+
+class GalleryDetail(RetrieveAPIView):
+    serializer_class = GallerySerializer
+    queryset = GalleryItem.objects.filter(published=True)
+    lookup_url_kwarg = 'slug'
+    lookup_field = 'slug'
