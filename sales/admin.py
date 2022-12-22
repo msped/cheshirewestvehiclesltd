2ee2b847028bfadd215f352269dd19ea37d163a5
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-22 14:02:31 +0000

    feat: added sales models to the admin site

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Vehicle, VehicleImages, Reservations, TradeIn
 
 # Register your models here.
+
+admin.site.register(Reservations)
+admin.site.register(TradeIn)
+
+class VehicleImagesInlineAdmin(admin.TabularInline):
+    model = VehicleImages
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    inlines = [VehicleImagesInlineAdmin]
+
+    class Meta:
+        model = Vehicle
+
+@admin.register(VehicleImages)
+class VehicleImagesAdmin(admin.ModelAdmin):
+    pass
