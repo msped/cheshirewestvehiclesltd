fb86c9d75e3c7bdcc90e6dd2b4a8821e33e037f9
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-22 14:12:52 +0000

    feat: added VehicleDetail to sale views

--- sales/views.py
+++ sales/views.py
@@ -1,10 +1,16 @@
 from django.db.models import Q
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from .models import Vehicle
-from .serializers import VehicleSerializerList
+from .serializers import VehicleSerializerList, VehicleSerializer
 
 class ListVehicles(ListAPIView):
     serializer_class = VehicleSerializerList
     queryset = Vehicle.objects.filter(Q(reserved='1') | Q(reserved='2'))
     paginate_by = 10
+
+class VehicleDetail(RetrieveAPIView):
+    serializer_class = VehicleSerializer
+    lookup_field = 'slug'
+    lookup_url_kwarg = 'slug'
+    queryset = Vehicle.objects.all()
