9b92ca5871234ee6eab2a40f2b96cc770fc17274
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-22 14:13:15 +0000

    feat: added vehicledetail to sales urls

--- sales/urls.py
+++ sales/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ListVehicles
+from .views import ListVehicles, VehicleDetail
 
 urlpatterns = [
     path('', ListVehicles.as_view(), name="list_of_vehicles"),
+    path('<str:slug>/', VehicleDetail.as_view(), name="vehicle_detail"),
 ]
