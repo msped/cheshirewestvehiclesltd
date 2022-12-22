260bf22cb87cee70454bb9868650ca3e564562e6
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-22 14:57:43 +0000

    refactor: removed VehicleListSerializer

--- sales/serializers.py
+++ sales/serializers.py
@@ -52,47 +52,6 @@ class VehicleSerializer(serializers.ModelSerializer):
             "images"
         ]
 
-class VehicleSerializerList(serializers.ModelSerializer):
-    images = serializers.SerializerMethodField()
-    reserved = serializers.CharField(
-        source="get_reserved_display"
-    )
-    car_state = serializers.CharField(
-        source="get_car_state_display"
-    )
-    fuel = serializers.CharField(
-        source="get_fuel_display"
-    )
-    body_type = serializers.CharField(
-        source="get_body_type_display"
-    )
-
-    def get_images(self, obj):
-        images = VehicleImages.objects.get(vehicle_id=obj.id, order_of_images=1)
-        serializer = VehicleImagesSerializer(images, many=False)
-        return serializer.data
-
-    class Meta:
-        model = Vehicle
-        fields = [
-            "id",
-            "slug",
-            "make",
-            "model",
-            "trim",
-            "year",
-            "fuel",
-            "body_type",
-            "car_state",
-            "reserved",
-            "mileage",
-            "engine_size",
-            "mot_expiry",
-            "extras",
-            "price",
-            "images"
-        ]
-
 class VehicleStateSerializer(serializers.ModelSerializer):
     reserved = serializers.CharField(
         source="get_reserved_display"
