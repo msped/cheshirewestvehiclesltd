89128b1d6436b13180cb388375f711205377906c
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-22 14:36:11 +0000

    feat: added vehicle state url

--- sales/urls.py
+++ sales/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ListVehicles, VehicleDetail
+from .views import ListVehicles, VehicleDetail, VehicleState
 
 urlpatterns = [
     path('', ListVehicles.as_view(), name="list_of_vehicles"),
     path('<str:slug>/', VehicleDetail.as_view(), name="vehicle_detail"),
+    path('<int:vehicle_id>/', VehicleState.as_view(), name="vehicle_state"),
 ]
