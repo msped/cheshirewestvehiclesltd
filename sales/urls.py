33c5594e4d03dd5e3e17fa76083576126faefcdf
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-25 00:17:09 +0000

    feat: added webhook uri and refactor reserve uri

--- sales/urls.py
+++ sales/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
 
-from .views import ListVehicles, VehicleDetail, VehicleState, ReserveVehicle
+from .views import (
+    ListVehicles,
+    VehicleDetail,
+    VehicleState,
+    StripePaymentIntentReserveVehicle,
+    stripe_webhook
+)
 
 urlpatterns = [
     path('', ListVehicles.as_view(), name="list_of_vehicles"),
     path('<str:slug>/', VehicleDetail.as_view(), name="vehicle_detail"),
     path('state/<int:vehicle_id>/', VehicleState.as_view(), name="vehicle_state"),
-    path('reserve/<int:vehicle_id>/', ReserveVehicle.as_view(), name="reserve_vehicle"),
+    path(
+        'reserve/<int:vehicle_id>/',
+        StripePaymentIntentReserveVehicle.as_view(),
+        name="payment_intent_reserve_vehicle"
+    ),
+    path('webhooks/stripe/', stripe_webhook, name='stripe-webhook'),
 ]
