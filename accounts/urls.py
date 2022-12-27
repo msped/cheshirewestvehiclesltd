14fa9d23f0b123842e152c4b06c9a55a9b381941
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-27 22:10:09 +0000

    feat: added change password uri

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
 
+from .views import ChangePasswordView
+
 urlpatterns = [
     path('', include('djoser.urls.jwt')),
     path('', include('djoser.urls')),
+    path(
+        'change-password/',
+        ChangePasswordView.as_view(),
+        name="change_password"
+    ),
 ]
