e400df48028e320c528aec7cf9ba1cf2507bb1e8
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-27 18:37:27 +0000

    feat: added BlacklistTokenView

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+class BlacklistTokenView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh']
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response(error, status=status.HTTP_400_BAD_REQUEST)
