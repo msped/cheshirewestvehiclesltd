502090650f33790173be153333388d8b470eb348
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-27 22:10:32 +0000

    feat: added blacklisttoken uri

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from .views import ChangePasswordView
+from .views import ChangePasswordView, BlacklistTokenView
 
 urlpatterns = [
     path('', include('djoser.urls.jwt')),
     path('', include('djoser.urls')),
+    path('jwt/blacklist/', BlacklistTokenView.as_view(), name="logout"),
     path(
         'change-password/',
         ChangePasswordView.as_view(),
