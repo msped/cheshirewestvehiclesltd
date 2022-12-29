492a4058d7412fad204123ab36766edefe9f4471
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-29 18:01:40 +0000

    feat: added CreateInvoice view

--- business_admin/views.py
+++ business_admin/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .utils import invoice_handler
 
 # Create your views here.
+
+class CreateInvoice(APIView):
+    def post(self, request):
+        email = invoice_handler(request.data)
+        if email:
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
