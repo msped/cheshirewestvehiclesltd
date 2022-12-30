638020a43f64a326b5814d114a93a6ee1b0a134e
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-30 23:18:45 +0000

    feat: added vehicle uri to create and list vehicles

--- business_admin/urls.py
+++ business_admin/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CreateInvoice
+from .views import CreateInvoice, CreateListVehicle
 
 urlpatterns = [
     path('invoice/', CreateInvoice.as_view(), name="create_invoice"),
+    path('vehicle/', CreateListVehicle.as_view(), name="create_vehicle"),
 ]
