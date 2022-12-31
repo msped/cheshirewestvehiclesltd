3dee66fe34f7e7f3b44eab418f66fe1385e21d12
Matt Edwards <mattsp.edwards@googlemail.com>
2022-12-31 21:49:35 +0000

    feat: added createlistvehicle for business admin

--- business_admin/views.py
+++ business_admin/views.py
@@ -1,13 +1,28 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from sales.models import Vehicle
+from sales.serializers import (
+    VehicleSerializer
+)
 from .utils import invoice_handler
 
 # Create your views here.
 
 class CreateInvoice(APIView):
+    permission_classes = [IsAdminUser]
+
     def post(self, request):
         email = invoice_handler(request.data)
         if email:
             return Response(status=status.HTTP_200_OK)
         return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class CreateListVehicle(ListCreateAPIView):
+    queryset = queryset = Vehicle.objects.all()
+    pagination_by = 10
+    serializer_class = VehicleSerializer
+    permission_classes = [IsAdminUser]
