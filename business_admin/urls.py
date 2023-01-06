d5be7241b4c13fc4a8744303a112dfa2b331993b
Matt Edwards <mattsp.edwards@googlemail.com>
2023-01-06 22:48:59 +0000

    feat: added gallery item uri

--- business_admin/urls.py
+++ business_admin/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CreateInvoice, CreateListVehicle
+from .views import CreateInvoice, CreateListVehicle, CreateListGalleryItem
 
 urlpatterns = [
     path('invoice/', CreateInvoice.as_view(), name="create_invoice"),
     path('vehicle/', CreateListVehicle.as_view(), name="create_vehicle"),
+    path('gallery/', CreateListGalleryItem.as_view(), name="create_gallery_item"),
 ]
