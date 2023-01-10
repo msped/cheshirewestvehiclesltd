a1cb8bd74bf09d7365d702ebbe4eee5e1e81bbe3
Matt Edwards <mattsp.edwards@googlemail.com>
2023-01-10 20:14:42 +0000

    refactor: override GallerySerializer create method to upload images

--- gallery/serializers.py
+++ gallery/serializers.py
@@ -31,8 +31,17 @@ class GalleryImageSerializer(serializers.ModelSerializer):
 
 class GallerySerializer(serializers.ModelSerializer):
     """Serializer for a full gallery item"""
-    slug = serializers.ReadOnlyField()
     images = serializers.SerializerMethodField()
+    uploaded_images = serializers.ListField(
+        child = serializers.FileField(
+            max_length=1000000,
+            allow_empty_file=False,
+            use_url=False
+        ),
+        write_only=True,
+        required=False
+    )
+
 
     class Meta:
         model = GalleryItem
@@ -46,9 +55,23 @@ class GallerySerializer(serializers.ModelSerializer):
             'description',
             'published',
             'images',
+            'uploaded_images',
         ]
 
     def get_images(self, obj):
         images = GalleryImage.objects.filter(item_id=obj.id)
         serializer = GalleryImageSerializer(images, many=True)
         return serializer.data
+
+    def create(self, validated_data):
+        if 'uploaded_images' in validated_data:
+            uploaded_images = validated_data.pop('uploaded_images')
+            gallery = GalleryItem.objects.create(**validated_data)
+            for image in uploaded_images:
+                GalleryImage.objects.create(
+                    item_id=gallery.id,
+                    image=image
+                )
+        else:
+            gallery = GalleryItem.objects.create(**validated_data)
+        return gallery
