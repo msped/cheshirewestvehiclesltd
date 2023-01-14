01224f4038fded78b68926ec62e3960106c37761
Matt Edwards <mattsp.edwards@googlemail.com>
2023-01-14 16:31:47 +0000

    feat: added GetUpdateDeleteGallery url

--- business_admin/urls.py
+++ business_admin/urls.py
@@ -4,12 +4,14 @@ from .views import (
     CreateInvoice,
     CreateListVehicle,
     CreateListGalleryItem,
-    GetUpdateDeleteVehicle
+    GetUpdateDeleteVehicle,
+    GetUpdateDeleteGallery
 )
 
 urlpatterns = [
     path('invoice/', CreateInvoice.as_view(), name="create_invoice"),
     path('vehicle/', CreateListVehicle.as_view(), name="create_vehicle"),
-    path('vehicle/<slug:slug>/', GetUpdateDeleteVehicle.as_view(), name="delete_vehicle"),
+    path('vehicle/<slug:slug>/', GetUpdateDeleteVehicle.as_view(), name="vehicle_options"),
     path('gallery/', CreateListGalleryItem.as_view(), name="create_gallery_item"),
+    path('gallery/<slug:slug>/', GetUpdateDeleteGallery.as_view(), name="gallery_options"),
 ]
