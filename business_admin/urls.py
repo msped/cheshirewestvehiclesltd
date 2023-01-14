482b6a143732a967dcda55f0596a428b3ff851ed
Matt Edwards <mattsp.edwards@googlemail.com>
2023-01-14 15:50:34 +0000

    feat: added url for get update destroy vehicle

--- business_admin/urls.py
+++ business_admin/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from .views import CreateInvoice, CreateListVehicle, CreateListGalleryItem
+from .views import (
+    CreateInvoice,
+    CreateListVehicle,
+    CreateListGalleryItem,
+    GetUpdateDeleteVehicle
+)
 
 urlpatterns = [
     path('invoice/', CreateInvoice.as_view(), name="create_invoice"),
     path('vehicle/', CreateListVehicle.as_view(), name="create_vehicle"),
+    path('vehicle/<slug:slug>/', GetUpdateDeleteVehicle.as_view(), name="delete_vehicle"),
     path('gallery/', CreateListGalleryItem.as_view(), name="create_gallery_item"),
 ]
