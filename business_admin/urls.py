48cb732cdacb68bc9d782b41515833c20145ec92
Matt Edwards <mattsp.edwards@googlemail.com>
2023-01-15 16:54:10 +0000

    feat :added create delte veihcle image url

--- business_admin/urls.py
+++ business_admin/urls.py
@@ -6,13 +6,19 @@ from .views import (
     CreateListGalleryItem,
     GetUpdateDeleteVehicle,
     GetUpdateDeleteGallery,
-    CreateDeleteGalleryImage
+    CreateDeleteGalleryImage,
+    CreateDeleteVehicleImage
 )
 
 urlpatterns = [
     path('invoice/', CreateInvoice.as_view(), name="create_invoice"),
     path('vehicle/', CreateListVehicle.as_view(), name="create_vehicle"),
     path('vehicle/<slug:slug>/', GetUpdateDeleteVehicle.as_view(), name="vehicle_options"),
+    path(
+        'vehicle/image/<int:object_id>/',
+        CreateDeleteVehicleImage.as_view(),
+        name="vehicle_image_options"
+    ),
     path('gallery/', CreateListGalleryItem.as_view(), name="create_gallery_item"),
     path('gallery/<slug:slug>/', GetUpdateDeleteGallery.as_view(), name="gallery_options"),
     path(
