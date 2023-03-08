a188b7565ecf880c6a5d2d850d730aee0f585a24
Matt Edwards <mattsp.edwards@googlemail.com>
2023-03-08 21:10:19 +0000

    build: added auditlog to installed apps and middleware

--- backend/settings.py
+++ backend/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'phonenumber_field',
     'rest_framework',
     'django_filters',
+    'auditlog',
     'corsheaders',
     'accounts',
     'business_admin',
@@ -65,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'auditlog.middleware.AuditlogMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
