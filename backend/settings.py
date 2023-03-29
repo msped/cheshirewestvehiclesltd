48f6355273b0b8bc0e51abdf90a95d34eb21c676
Matt Edwards <mattsp.edwards@googlemail.com>
2023-03-29 23:54:45 +0100

    feat: added test database name to database config

--- backend/settings.py
+++ backend/settings.py
@@ -102,6 +102,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
+        'TEST': {
+            'NAME': 'testdatabase'
+        }
     }
 }
 
